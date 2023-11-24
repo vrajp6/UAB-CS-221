3f6f7efe068bb125d2189675e0ed7032be250183
vrajp6 <vrajpatel2021@gmail.com>
2023-11-24 14:36:14 -0600

    update

--- Lab 11/script.js
+++ Lab 11/script.js
@@ -12,18 +12,17 @@ function searchForBooks() {
     }
 
     // used information from lecture slides and ChatGPT to write this part
-    /* ChatGPT Prompt: If I was asked to implement a function to make a request to the Google Books APILinks to an external site when the user searches for books using the search button, then
+    /* ChatGPT Prompt: If I was asked to implement a function to make a request to the Google Books API when the user searches for books using the search button, then
      process the API response to retrieve book data, how would I write that in JavaScript. */
-     // creating the API URL by encoding the search text and adding it to the end of the URL
      // used this link to understand the Fetch API before proceeding: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
      // creating the API URL by encoding the search text and adding it to the end of the URL
     const bookAPIUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(searchText)}`;
 
     // fetching the data from the API URL
     fetch(bookAPIUrl)
-        .then(response => response.json()) // Converting the response to JSON
-        .then(data => showBooks(data.items)) // Passing the book data to the showBooks function
-        .catch(error => console.error("Error:", error)); // Logging any errors to the console
+        .then(response => response.json()) // converting the response to JSON
+        .then(data => showBooks(data.items)) // passing the book data to the showBooks function
+        .catch(error => console.error("Error:", error)); // logging any errors to the console
 }
 
 // this function displays the books in the browser
@@ -40,11 +39,20 @@ function showBooks(books) {
         // creating a new h3 element for the book title and setting its text
         const title = document.createElement('h3');
         title.textContent = book.volumeInfo.title;
+        bookItem.appendChild(title);
         // creating a button to toggle the display of additional details
         const button = document.createElement('button');
         button.textContent = 'Details';
         // when the button is clicked, call the showDetails function to show/hide book details
         button.onclick = () => showDetails(bookItem, book.volumeInfo);
+        bookItem.appendChild(button);
+        
+        // creating a button to add the book to the bookshelf
+        const addButton = document.createElement('button');
+        addButton.textContent = 'Add to Bookshelf';
+        // when the button is clicked, call the addToBookshelf function to add the book to the bookshelf
+        addButton.onclick = () => addToBookshelf(book);
+        bookItem.appendChild(addButton);
         
         // adding the title and button to the book item div
         bookItem.appendChild(title);
@@ -77,3 +85,57 @@ function showDetails(bookItem, details) {
         bookItem.appendChild(detailsDiv);
     }
 }
+
+// BONUS: Bookshelf - used ChatGPT prompt to help write this part, is not fully working, but still wanted partial bonus points
+/* ChatGPT Prompt: How could I edit my current code so that I can create a bookshelf that stores books that the user has added? This bookshelf should be saved to local storage, 
+so a refresh of the page will not remove it. Also, the user should be able to view their bookshelf, see details of the book within it, and remove books from it. */
+// you are able to add and remove books from the bookshelf, and when you refresh the page, the bookshelf is still there, but I could not get the details to show up when you click the button
+
+// this function adds a book to the bookshelf
+function addToBookshelf(book) {
+    let bookshelf = JSON.parse(localStorage.getItem('bookshelf')) || [];
+    // checking if the book is already in the bookshelf
+    bookshelf.push(book);
+    localStorage.setItem('bookshelf', JSON.stringify(bookshelf));
+    displayBookshelf(); 
+}
+// this function displays the bookshelf
+function displayBookshelf() {
+    const bookshelfList = document.getElementById('bookshelfList');
+    bookshelfList.innerHTML = '';
+    
+    // get the bookshelf from local storage, or an empty array if it's not set yet
+    let bookshelf = JSON.parse(localStorage.getItem('bookshelf')) || [];
+    bookshelf.forEach(book => {
+        // create a new list item for each book and add it to the bookshelf list
+        const li = document.createElement('li');
+        li.textContent = book.volumeInfo.title;
+
+        // create a button to view details and remove the book
+        const viewDetailsButton = document.createElement('button');
+        viewDetailsButton.textContent = 'View Details';
+        viewDetailsButton.onclick = () => showDetails(book.volumeInfo);
+        li.appendChild(viewDetailsButton);
+        
+        // create a button to remove the book from the bookshelf
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.onclick = () => removeFromBookshelf(book);
+        li.appendChild(removeButton);
+        
+        bookshelfList.appendChild(li);
+    });
+}
+
+// this function removes a book from the bookshelf
+function removeFromBookshelf(bookToRemove) {
+    let bookshelf = JSON.parse(localStorage.getItem('bookshelf')) || [];
+    // filter out the book to remove
+    bookshelf = bookshelf.filter(book => book.id !== bookToRemove.id);
+    // save the updated bookshelf to local storage
+    localStorage.setItem('bookshelf', JSON.stringify(bookshelf));
+    displayBookshelf(); // update the bookshelf display
+}
+
+// calling displayBookshelf on page load to show the stored bookshelf
+window.onload = displayBookshelf;
